acccc0d0e3d0de578dd825e8151f412592d03eb1
Fernanda <fernandasousacampos6@gmail.com>
2025-04-27 12:03:19 -0300

    Exercicio5.py

--- Dorivas/Exercicio5.py
+++ Dorivas/Exercicio5.py
@@ -1,12 +1,16 @@
-num = int(input("digite um numero:"))
+num = input("Digite um número: ")
 
-contador = 0
+if num.isdigit():
+    num = int(num)
+    contador = 0
 
-for i in range(1, num + 1):
-    if num % i == 0:
-        contador += 1
+    for i in range(1, num + 1):
+        if num % i == 0:
+            contador += 1
 
-if contador == 2:
+    if contador == 2:
         print(f"{num} é primo")
+    else:
+        print(f"{num} não é primo")
 else:
-        print(f"{num}não é primo")
+    print("INVALIDO")
