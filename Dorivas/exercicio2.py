f3697f3c078e8bc47f7131e0402d179a46d23bed
Fernanda <fernandasousacampos6@gmail.com>
2025-04-27 12:01:20 -0300

    exercicio2.py

--- Dorivas/exercicio2.py
+++ Dorivas/exercicio2.py
@@ -1,10 +1,14 @@
-num = int(input("Digite um numero:"))
+num = input("Digite um número: ")
 
-soma = 0
-contador = 0
+if num.isdigit():
+    num = int(num)
+    soma = 0
+    contador = 0
 
-while contador <= num:
-    soma += contador
-    contador += 1
+    while contador <= num:
+        soma += contador
+        contador += 1
 
-print(f"A soma de 1 até {num} é: {soma}")
+    print(f"A soma de 1 até {num} é: {soma}")
+else:
+    print("INVALIDO")
