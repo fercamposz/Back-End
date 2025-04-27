3cfe9fc38e92a7fe355b529feb636bbcb84013fe
Fernanda <fernandasousacampos6@gmail.com>
2025-04-27 12:06:41 -0300

    exercicio8.py

--- Dorivas/exercicio8.py
+++ Dorivas/exercicio8.py
@@ -1,6 +1,11 @@
-num = int(input("digite o numero de termo:"))
-soma = 0
-for i in range(1, num + 1):
-    soma += 1/ i
+num = input("Digite o número de termos: ")
 
-print(f"a soma da serie harmônica é:{soma:.2f}")
+if num.isdigit():
+    num = int(num)
+    soma = 0
+    for i in range(1, num + 1):
+        soma += 1 / i
+
+    print(f"A soma da série harmônica é: {soma:.2f}")
+else:
+    print("INVALIDO")
